f1c310b54d56877f3f4e4b2d8c9639885ae4a564
Stephen O'Hair <stephen.ohair@gmail.com>
2014-04-15 17:26:45 +1000

    add back cli

---
+++ MediaBeaver/MediaBeaverCli/src/main/java/com/ibus/mediabeaver/cli/Main.java
@@ -0,0 +1,28 @@
+package com.ibus.mediabeaver.cli;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.ibus.mediabeaver.cli.utility.MediaManager;
+import com.ibus.mediabeaver.core.data.DataInitialiser;
+import com.ibus.mediabeaver.core.data.HibernateUtil;
+
+public class Main
+{
+	static Logger log = Logger.getLogger(Main.class.getName());
+	
+	public static void main(String[] args)
+	{
+		log.debug("initialising data");
+		DataInitialiser.Initialise();
+		
+		
+		log.debug("Starting media movement");
+		MediaManager h = new MediaManager();
+		h.moveMedia();
+
+		
+	}
+
+}
