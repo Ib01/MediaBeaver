e87d438868dcbdea2443a416b83244bb39c4d985
ib_ross <ib.ross01@gmail.com>
2014-09-24 16:56:26 +1000

    threading jetty start and stop

--- MediaBeaverServer/src/main/java/Main.java
+++ MediaBeaverServer/src/main/java/Main.java
@@ -1,4 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.security.ProtectionDomain;
 
 import org.apache.log4j.Logger;
@@ -13,37 +21,81 @@ import org.eclipse.jetty.webapp.WebAppContext;
  * */
 public class Main 
 {
-	static Logger log = Logger.getLogger(Main.class.getName());
-  
+	public static final String StartServer = "-start";
+	public static final String StopServer = "-stop";
+	private static Logger log = Logger.getLogger(Main.class.getName());
+	private static Server jettyServer;
+	
+	private static class MonitorThread extends Thread 
+	{
+        private ServerSocket socket;
+ 
+        public MonitorThread() {
+            setDaemon(true);
+            setName("StopMonitor");
+            try {
+                socket = new ServerSocket(8079, 1, InetAddress.getByName("127.0.0.1"));
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+ 
+        @Override
+        public void run() {
+            System.out.println(">>> running jetty 'stop' thread");
+            Socket accept;
+            
+            try {
+                accept = socket.accept();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+                reader.readLine();
+                
+                System.out.println(">>> stopping jetty embedded server");
+                
+                jettyServer.stop();
+                accept.close();
+                socket.close();
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+	
+	
+	
 	public static void main(String[] args) 
 	{	
-		//DataInitialiser.Initialise();
-		/*String action = Args.getAction(args);
+		if(args[0].equalsIgnoreCase(StartServer))
+			startJetty();
 		
-		if(action.equals(Args.StartServer))
-		{
-			WebServer.start();
-		}
-		else if(action.equals(Args.ProcessMedia))
-		{
-			MediaManager m = new MediaManager();
-			m.MoveMedia();
-		} */
 		
 		startJetty();
 		
 	}
 
+	
+	public static void stopJetty() throws UnknownHostException, IOException
+	{
+		Socket s = new Socket(InetAddress.getByName("127.0.0.1"), 8079);
+        OutputStream out = s.getOutputStream();
+        
+        log.debug(">>> Sending jetty stop request");
+        
+        out.write(("\r\n").getBytes());
+        out.flush();
+        s.close();
+	}
+	
 	public static void startJetty()
 	{
 		try 
 		{
 			log.debug("Starting Jetty Server");
-			Server server = new Server();
+			jettyServer = new Server();
 
 		    SelectChannelConnector connector = new SelectChannelConnector();
 		    connector.setPort(8081);
-		    server.addConnector(connector);
+		    jettyServer.addConnector(connector);
 
 		    ProtectionDomain domain = Main.class.getProtectionDomain();
 		    URL location = domain.getCodeSource().getLocation();
@@ -61,11 +113,11 @@ public class Main
 		    handlers.setHandlers(new Handler[] {webapp, new ShutdownHandler(server, ShutdownPassword)});
 		    server.setHandler(handlers);*/
 		    
-		    server.setHandler(webapp);
-		    server.start();
+		    jettyServer.setHandler(webapp);
+		    jettyServer.start();
 			
 		    log.debug("Joinging Jetty Server");
-		    server.join();
+		    jettyServer.join();
 		    		
 		} catch (Exception e) {
 			log.error("En error occured while starting the jetty web server", e);
@@ -77,7 +129,14 @@ public class Main
 	
 	
 	
-	
+	public static void showUsage()
+	{
+		System.out.println("");
+		System.out.println("Usage: ");
+		System.out.println("MediaBeaver "+StartServer);
+		System.out.println("MediaBeaver "+StopServer);
+		
+	}
 	
 	
 
